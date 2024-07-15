65c4f5732d1c80acaa974a1566fb5652790c805c
spanovs01 <spanovs01frag@egmail.com>
2024-07-15 12:51:38 +0300

    modified:   Soccer/Vision/lookARUCO.py 	modified:   Soccer/strategy.py

--- Soccer/strategy.py
+++ Soccer/strategy.py
@@ -869,11 +869,11 @@ class Player():
             # write motion parameters to zubr-controller motion
             intercom.memISet(var.orderFromHead, 0)              #  0 - no order, 1 - straight forward, 2 - to left, 3- to right, 4 - reverse back
             intercom.memISet(var.cycle_number, 33)
-            intercom.memISet(var.hipTilt, 80)
+            intercom.memISet(var.hipTilt, 300)
             intercom.memISet(var.fps, 2)
-            intercom.memISet(var.stepLengthOrder, 40)
+            intercom.memISet(var.stepLengthOrder, 30)
             intercom.memISet(var.gaitHeight, 135)
-            intercom.memISet(var.stepHeight, 35)
+            intercom.memISet(var.stepHeight, 30)
             intercom.memISet(var.pitStop, 1)                    # 1 - go on, 0 - stop waiting
             labels = [[], [], [], ['start'], []]
             pressed_button = self.motion.push_Button(labels)
